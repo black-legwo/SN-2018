517544a8283446b5ae757ba8713118e642e9148c
Mag-L-B <40523316+Mag-L-B@users.noreply.github.com>
2018-11-20 16:01:54 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+using namespace std;
+
+void equations(double* v, double* r, double* delta_m, double* rho, double* P, double* e, double dt){
+    *(delta_m + 1) = (4*M_PI/3)*(*(r+1)**3 - *(r)**3)*(*rho)
+
+
+}
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
